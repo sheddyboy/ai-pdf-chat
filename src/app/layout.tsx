5e7b2edb0d9505f92a409ae035749fcb36d94e91
personal <shedrackitulah@gmail.com>
2024-08-20 16:09:44 +0100

    feat: update home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "sonner";
+import ReactQueryProvider from "@/providers/reactQuery";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} h-full overflow-hidden`}>
+        <ReactQueryProvider>{children}</ReactQueryProvider>
+        <Toaster richColors />
+      </body>
     </html>
   );
 }
