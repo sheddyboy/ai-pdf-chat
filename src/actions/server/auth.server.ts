c3f5b8e55ac76cfa6149c29eb8a091bcf5824dac
personal <shedrackitulah@gmail.com>
2024-08-20 15:59:22 +0100

    feat: supabase setup

---
+++ src/actions/server/auth.server.ts
@@ -0,0 +1,58 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+import { createClient } from "@/lib/supabase/server";
+import { z } from "zod";
+import { loginFormSchema, signUpFormSchema } from "@/schemas";
+
+export async function login(values: z.infer<typeof loginFormSchema>) {
+  const result = loginFormSchema.safeParse(values);
+  if (result.error) {
+    throw new Error(result.error.message);
+  }
+
+  const supabase = createClient();
+
+  const { error, data } = await supabase.auth.signInWithPassword({
+    email: result.data.email,
+    password: result.data.password,
+  });
+
+  if (error) {
+    throw new Error(error.message);
+  }
+  revalidatePath("/", "layout");
+  return data;
+}
+
+export async function signUp(values: z.infer<typeof signUpFormSchema>) {
+  const result = signUpFormSchema.safeParse(values);
+  if (result.error) {
+    throw new Error(JSON.stringify(result.error));
+  }
+  const supabase = createClient();
+
+  const { error, data } = await supabase.auth.signUp({
+    email: result.data.email,
+    password: result.data.password,
+  });
+
+  if (error) {
+    throw new Error(error.message);
+  }
+  revalidatePath("/", "layout");
+  return data;
+}
+
+export async function logOut() {
+  const supabase = createClient();
+
+  const { error } = await supabase.auth.signOut();
+
+  if (error) throw new Error(error.message);
+
+  revalidatePath("/", "layout");
+  redirect("/");
+}
---
+++ supabase/seed.sql
@@ -0,0 +1,28 @@
+create function public.handle_new_user () returns trigger as $$
+begin
+  insert into public.profiles (id,name,profile_picture)
+  values (new.id, new.raw_user_meta_data ->> 'name',new.raw_user_meta_data ->> 'picture');
+  return new;
+end;
+$$ language plpgsql security definer;
+
+create trigger on_auth_user_created
+after insert on auth.users for each row
+execute procedure public.handle_new_user ();
+
+-- create a bucket.
+
+insert into storage.buckets
+  (id,name,public)
+values
+  ('images','images',true);
+
+
+CREATE POLICY "Give images access to own folder 1ufimg_0" ON storage.objects FOR SELECT TO public USING (bucket_id = 'images' AND auth.uid()::text = (storage.foldername(name))[1]);
+CREATE POLICY "Give images access to own folder 1ufimg_1" ON storage.objects FOR INSERT TO public WITH CHECK (bucket_id = 'images' AND auth.uid()::text = (storage.foldername(name))[1]);
+CREATE POLICY "Give images access to own folder 1ufimg_2" ON storage.objects FOR UPDATE TO public USING (bucket_id = 'images' AND auth.uid()::text = (storage.foldername(name))[1]);
+CREATE POLICY "Give images access to own folder 1ufimg_3" ON storage.objects FOR DELETE TO public USING (bucket_id = 'images' AND auth.uid()::text = (storage.foldername(name))[1]);
+
+
+-- Enable MODDATETIME extension
+create extension if not exists moddatetime schema extensions;
